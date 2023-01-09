5923cb5cab2918e1ae95ea3ee956396fecc29dc4
drgorillamd <dr_gorilla@protonmail.com>
2023-01-09 16:55:45 +0100

    timestamp

--- actions-test/readBalance-test-local.ts
+++ actions-test/readBalance-test-local.ts
@@ -3,7 +3,7 @@ import { TestRuntime } from "@tenderly/actions-test";
 import { readBalance } from "../actions/readBalance";
 import { BigNumber, ethers } from "ethers";
 
-import payload from "./payloads/pay.json";
+import payPayload from "./payloads/pay.json";
 
 const _jbV3EthTerminal: string = "0x594Cb208b5BB48db1bcbC9354d1694998864ec63";
 
@@ -24,14 +24,17 @@ const main = async () => {
   );
 
   // We initialize the difference to 0
-  testRuntime.context.storage.putJson("difference-" + payload.blockNumber, "0");
+  testRuntime.context.storage.putJson(
+    "difference-" + payPayload.blockNumber,
+    "0"
+  );
 
   // Execute the action based on a single transaction with a Pay event of 0xc6f3b40b6c0000
-  await testRuntime.execute(readBalance, payload);
+  await testRuntime.execute(readBalance, payPayload);
 
   console.log(
     await testRuntime.context.storage.getJson(
-      "difference-" + payload.blockNumber
+      "difference-" + payPayload.blockNumber
     )
   );
 };
