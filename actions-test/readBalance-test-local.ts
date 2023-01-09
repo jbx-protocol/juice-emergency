467d067390d40ae4830db7bf4d3fa089465fb482
drgorillamd <dr_gorilla@protonmail.com>
2023-01-09 15:29:41 +0100

    feels better

--- actions-test/readBalance-test-local.ts
+++ actions-test/readBalance-test-local.ts
@@ -1,14 +1,28 @@
 //import { expect } from "chai";
 import { TestRuntime } from "@tenderly/actions-test";
 import { readBalance } from "../actions/readBalance";
+import { BigNumber, ethers } from "ethers";
+
+const _jbV3EthTerminal: string = "0x594Cb208b5BB48db1bcbC9354d1694998864ec63";
 
 const main = async () => {
+  const provider = new ethers.providers.JsonRpcProvider(
+    "https://rpc.ankr.com/eth"
+  );
+
   const testRuntime = new TestRuntime();
+  const balanceInWei: BigNumber = await provider.getBalance(_jbV3EthTerminal);
 
   // We set an arbitrary previous balance
-  testRuntime.context.storage.putJson("balance", "1343556667419709741021");
+  testRuntime.context.storage.putJson(
+    "balance",
+    balanceInWei.add(1).toString()
+  );
 
-  // Execute the action based on a single transaction with a
+  // We initialize the difference to 0
+  testRuntime.context.storage.putJson("difference", "0");
+
+  // Execute the action based on a single transaction with a Pay event of 0xc6f3b40b6c0000
   await testRuntime.execute(readBalance, require("./payloads/pay.json"));
 
   console.log(await testRuntime.context.storage.getJson("difference"));
--- actions/readBalance.ts
+++ actions/readBalance.ts
@@ -6,6 +6,7 @@ import { abi as jbV3EthTerminalAbi } from "./artifacts/JBETHPaymentTerminal.json
 const provider = new ethers.providers.JsonRpcProvider(
   "https://rpc.ankr.com/eth"
 );
+const _jbV3EthTerminal: string = "0x594Cb208b5BB48db1bcbC9354d1694998864ec63";
 
 /**
  * @notice Reads the balance of the terminal contract, parse the following event
@@ -14,9 +15,7 @@ const provider = new ethers.providers.JsonRpcProvider(
  * @param context context passed by Tenderly
  */
 export const readBalance: ActionFn = async (context: Context, event: Event) => {
-  const _jbV3EthTerminal: string = "0x594Cb208b5BB48db1bcbC9354d1694998864ec63";
   const blockEvent = event as BlockEvent;
-
   const balanceInWei: BigNumber = await provider.getBalance(_jbV3EthTerminal);
   const previousBalance: BigNumber = ethers.BigNumber.from(
     await context.storage.getJson("balance")
@@ -25,7 +24,7 @@ export const readBalance: ActionFn = async (context: Context, event: Event) => {
   // Store the current balance to compare during next run
   await context.storage.putJson("balance", balanceInWei);
 
-  if (balanceInWei != previousBalance) {
+  if (!balanceInWei.eq(previousBalance)) {
     console.log("balance changed!");
     console.log("current balance: " + balanceInWei);
     console.log("previous balance: " + previousBalance);
@@ -38,23 +37,84 @@ export const readBalance: ActionFn = async (context: Context, event: Event) => {
       provider.getSigner(0)
     );
 
-    // Test with Pay for now
-    let eventFilter = terminalContract.filters.Pay();
-    let events = await terminalContract.queryFilter(
-      eventFilter,
+    // TODO: batch rpc calls
+
+    // Query each filter, as ethers doesn't suppoort combining filters
+    let payEvents = await terminalContract.queryFilter(
+      terminalContract.filters.Pay(),
       blockEvent.blockNumber,
       blockEvent.blockNumber
     );
 
+    let redeemEvents = await terminalContract.queryFilter(
+      terminalContract.filters.RedeemTokens(),
+      blockEvent.blockNumber,
+      blockEvent.blockNumber
+    );
+
+    let addToBalanceEvents = await terminalContract.queryFilter(
+      terminalContract.filters.AddToBalance(),
+      blockEvent.blockNumber,
+      blockEvent.blockNumber
+    );
+
+    let distributePayoutsEvents = await terminalContract.queryFilter(
+      terminalContract.filters.DistributePayouts(),
+      blockEvent.blockNumber,
+      blockEvent.blockNumber
+    );
+
+    let useAllowanceEvents = await terminalContract.queryFilter(
+      terminalContract.filters.UseAllowance(),
+      blockEvent.blockNumber,
+      blockEvent.blockNumber
+    );
+
+    let migrateEvents = await terminalContract.queryFilter(
+      terminalContract.filters.Migrate(),
+      blockEvent.blockNumber,
+      blockEvent.blockNumber
+    );
+
+    // Count the amounts which should enter/leave the terminal:
     let cumSum = ethers.BigNumber.from("0");
 
-    for (let i = 0; i < events.length; i++) {
-      cumSum += events[i].args?.amount;
+    // -- Adding to the terminal --
+    for (let i = 0; i < payEvents.length; i++) {
+      cumSum = cumSum.add(payEvents[i].args?.amount);
     }
 
+    for (let i = 0; i < addToBalanceEvents.length; i++) {
+      cumSum = cumSum.add(addToBalanceEvents[i].args?.amount);
+    }
+
+    // -- Removing from the terminal --
+    for (let i = 0; i < redeemEvents.length; i++) {
+      cumSum = cumSum.sub(redeemEvents[i].args?.reclaimedAmount);
+    }
+
+    for (let i = 0; i < distributePayoutsEvents.length; i++) {
+      // Counting if distributing to allocator or EOA only:
+      if (distributePayoutsEvents[i].args?.projectId != 0)
+        cumSum = cumSum.sub(distributePayoutsEvents[i].args?.amount);
+    }
+
+    for (let i = 0; i < useAllowanceEvents.length; i++) {
+      cumSum = cumSum.sub(useAllowanceEvents[i].args?.amount);
+    }
+
+    for (let i = 0; i < migrateEvents.length; i++) {
+      cumSum = cumSum.sub(migrateEvents[i].args?.amount);
+    }
+
+    // If the difference is not zero, trigger an alarm
     if (cumSum != balanceInWei) {
-      await context.storage.putJson("difference", balanceInWei.sub(cumSum));
+      await context.storage.putJson(
+        "difference",
+        balanceInWei.sub(previousBalance).sub(cumSum)
+      );
     }
 
+    console.log("cumSum: " + cumSum);
   }
 };
